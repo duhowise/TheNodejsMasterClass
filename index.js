073f501bea09d9d0d0fae0aee39594579608961b
duhowise <duhwise@gmail.com>
2020-02-03 22:48:26 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+* primary file for the API
+
+
+*/
+
+// Dependencies
+var http = require("http");
+var url = require("url");
+// the server should respond to all requests with a string
+
+var server = http.createServer(function(req, res) {
+    //get the url and pass it
+
+    var parsedUrl = url.parse(req.url, true);
+
+    //get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+    
+    //get the Http Method
+    var method = req.method.toLowerCase();
+
+    //get the headers as an object
+
+    var headers = req.headers;
+
+
+
+
+
+    //send the response
+    res.end("Hello world\n");
+
+    //log the request path
+    console.log(
+      "Request received on path: " +
+        trimmedPath +
+        " With method " +
+        method +
+        " and query string parameters ",
+      queryStringObject
+    );
+});
+// start the server and have it listen on port 3000
+server.listen(3000, function ()
+{
+    console.log("server started on port 3000");
+});
